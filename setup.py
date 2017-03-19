db096cf12262d02b4e26dcd9eaa27695d6bbc9e0
shortjared <jaredlshort@gmail.com>
2017-03-18 21:23:15 -0400

    version & dist bump

--- setup.py
+++ setup.py
@@ -1,25 +1,19 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE.txt') as f:
-    license = f.read()
-
 with open('requirements.txt') as f:
     requires = f.read()
 
 setup(
     name='hurricane_factory',
     description='Manage CloudFormation at Massive Scale',
-    version='0.1.1',
+    version='0.1.5',
     author='Jared Short',
     author_email='jaredlshort@gmail.com',
     url='https://github.com/trek10inc/hurricane-factory',
     install_requires=requires.split('\n'),
     scripts=['bin/hcf'],
     keywords=['aws', 'cloudformation', 'management', 'orchestration'],
-    license=license,
+    license='MIT',
     packages=find_packages(exclude=('tests', 'docs'))
 )
